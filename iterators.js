c1aafc709e532daa2bbe568e0d274af5d3bb54c3
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-01-11 17:24:13 +0300

    the .reduce iterator method

--- iterators.js
+++ iterators.js
@@ -61,3 +61,16 @@ const startsWithS = animals.findIndex(animal => {
   return animal[0] === 's'
 })
 console.log(startsWithS)
+
+
+
+// the .reduce() method
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue
+}, 10) // here 10 acts as a second argument
+
+console.log(newSum)
