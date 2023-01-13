e23d23e361bb1682f59d4ecf9b5df05790ee1429
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-01-13 18:34:15 +0300

    JavaScript Advanced Objects - Factory Functions

--- advanced_objects.js
+++ advanced_objects.js
@@ -62,4 +62,18 @@ const robot = {
   };
   robot.numOfSensors = 100
   console.log(robot.numOfSensors)
-  
+
+
+
+// Factory Functions
+const robotFactory = (model, mobile) => {
+    return {
+      model: model,
+      mobile: mobile,
+      beep() {
+        console.log('Beep Boop')
+      }
+    }
+  };
+  const tinCan = robotFactory('P-500', true)
+  tinCan.beep()
