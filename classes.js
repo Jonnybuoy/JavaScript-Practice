2ac4e07c483d73d37c3f1b69bfe05fc3cd2d1401
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-01-15 17:58:12 +0300

    JavaScript Classes - Declaration and Instantiation

--- classes.js
+++ classes.js
@@ -2,10 +2,28 @@
 
 class Surgeon {
     constructor(name, department) {
-      this.name = name;
-      this.department = department;
+      this._name = name;
+      this._department = department;
+      this._remainingVacationDays = 20;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    get department() {
+      return this._department;
+    }
+  
+    get remainingVacationDays() {
+      return this._remainingVacationDays;
+    }
+  
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays -= daysOff
     }
   }
   
+  
   const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
-  const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics')
+  const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics');
