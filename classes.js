114ab416776b503bf8c5073e2ec51bb92cff01ae
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-01-15 17:40:50 +0300

    JavaScript Classes - Declaration and Instantiation

---
+++ classes.js
@@ -0,0 +1,11 @@
+// Class Declaration and Instantiation
+
+class Surgeon {
+    constructor(name, department) {
+      this.name = name;
+      this.department = department;
+    }
+  }
+  
+  const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
+  const surgeonJackson = new Surgeon('Ruth Jackson', 'Orthopedics')
